132ab9f6e4af573a0c43b9c6fa37cf530dbbcc01
Dinh-Linh <linhnd26052004@gmail.com>
2024-08-19 13:58:21 +0700

    base

---
+++ android/weatherApp/app/src/main/java/com/example/weatherapp/fragment/WeatherDetailsFragment.kt
@@ -0,0 +1,27 @@
+package com.example.weatherapp.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.weatherapp.R
+import com.example.weatherapp.databinding.FragmentWeatherHomeBinding
+
+
+class WeatherDetailsFragment : Fragment() {
+
+    private val binding by lazy { FragmentWeatherHomeBinding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return binding.root
+    }
+
+
+}
